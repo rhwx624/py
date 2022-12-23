2d40190eb958db51420c64f291a9b66d373db8f3
office <ren.haowx@gmail.com>
2022-12-24 02:14:44 +0900

    network

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+def sigmoid(z):
+    return 1.0/(1.0+np.exp(-z))
+
+def feedforward(self, a):
+    """Return the output of the network if "a" is input."""
+    for b, w in zip(self.biases, self.weights):
+        a=sigmoid(np.dot(w, a)+b)
+
+print("hello world!")
